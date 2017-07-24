7a066251020350291a8031a1cea4998562afb431
agliullin <albertagliullin@gmail.com>
2017-07-24 17:39:28 +0300

    Top menu

---
+++ .top.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Главная", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О сайте", 
+		"about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Для работодателей", 
+		"employer/", 
+		Array(), 
+		Array(), 
+		"CSite::InGroup(array(7))" 
+	)
+);
+?>
