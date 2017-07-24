57d204abd0f0c11fa9b6f39cf4ddf72c8f5b5b75
agliullin <albertagliullin@gmail.com>
2017-07-24 23:08:46 +0300

    Edit urlrewrite page

--- urlrewrite.php
+++ urlrewrite.php
@@ -1,10 +1,22 @@
 <?
 $arUrlRewrite = array(
 	array(
+		"CONDITION" => "#^../vacancy/#",
+		"RULE" => "",
+		"ID" => "my:vacancy",
+		"PATH" => "/employer/index.php",
+	),
+	array(
 		"CONDITION" => "#^/vacancy/#",
 		"RULE" => "",
 		"ID" => "my:vacancy",
 		"PATH" => "/vacancy/index.php",
+	),
+	array(
+		"CONDITION" => "#^/employer/response/#",
+		"RULE" => "",
+		"ID" => "my:employer.response",
+		"PATH" => "/employer/response/index.php",
 	),
 );
 
