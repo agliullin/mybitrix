830a4b01c280601bf8cadcc707ea17a23895bccc
agliullin <albertagliullin@gmail.com>
2017-08-07 23:36:33 +0300

    Edit lib files

--- local/modules/job/lib/employer.php
+++ local/modules/job/lib/employer.php
@@ -9,33 +9,6 @@ class JobEmployer extends Entity\DataManager
         CModule::IncludeModule("iblock");
     }
     
-    public static function getFilePath()
-    {
-      return __FILE__;
-    }
-   
-    public static function getTableName()
-    {
-        return 'job_employer';
-    }
-
-    public static function getMap()
-    {
-        return array(
-            new Entity\IntegerField('ID', array(
-                'primary' => true,
-                'autocomplete' => true
-            )),
-            new Entity\StringField('NAME', array(
-                'required' => true,
-            )),
-            new Entity\StringField('SPECIALIZATION'),
-            new Entity\IntegerField('SALARY_FROM'),
-            new Entity\IntegerField('SALARY_UP_TO'),
-            new Entity\DatetimeField('TIME_ACTIVE')
-        );
-    }
-
     public function GetEmployerById($Id) {
         $EmployerCIBlockElement = CIBlockElement::GetById($Id);
         $EmployerElement = $EmployerCIBlockElement->GetNextElement();
--- local/modules/job/lib/vacancy.php
+++ local/modules/job/lib/vacancy.php
@@ -8,33 +8,6 @@ class JobVacancy extends Entity\DataManager
     function __construct() {
         CModule::IncludeModule("iblock");
     }
-    
-    public static function getFilePath()
-    {
-      return __FILE__;
-    }
-   
-    public static function getTableName()
-    {
-        return 'job_vacancy';
-    }
-
-    public static function getMap()
-    {
-        return array(
-            new Entity\IntegerField('ID', array(
-                'primary' => true,
-                'autocomplete' => true
-            )),
-            new Entity\StringField('NAME', array(
-                'required' => true,
-            )),
-            new Entity\StringField('SPECIALIZATION'),
-            new Entity\IntegerField('SALARY_FROM'),
-            new Entity\IntegerField('SALARY_UP_TO'),
-            new Entity\DatetimeField('TIME_ACTIVE')
-        );
-    }
 
     public function GetVacancyById($Id) {
         $VacancyCIBlockElement = CIBlockElement::GetById($Id);
